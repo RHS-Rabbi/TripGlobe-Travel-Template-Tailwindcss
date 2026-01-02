aac95c8830d9cda20806088ecbf37c79eaa699dc
RHS-Rabbi <rabbihossain187@gmail.com>
2026-01-02 19:37:31 +0600

    magnific js added

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,8 +13,15 @@ $(document).ready(function () {
 
 });
 
+// For Hero Section Rotating Text
 const text = document.querySelector('.hero-icon .hero-content-icon');
 text.innerHTML = text.innerText.split("").map(
     (char, i) =>
         `<span style= "transform:rotate(${i * 17}deg)">${char}</span>`
 ).join("")
+
+$(document).ready(function() {
+	$('.hero-play-icon').magnificPopup({
+    type: 'iframe'
+  });
+});
